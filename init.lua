4d0be4d93d648f358880b9988acc9e9d1e217e10
David Lu <davidboxboro@gmail.com>
2022-11-28 18:44:53 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,54 @@
+-- init.lua
+
+require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+    use 'EdenEast/nightfox.nvim'
+
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+    use 'neovim/nvim-lspconfig'
+   
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/vim-vsnip'
+end)
+
+-- Tabbing
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+
+-- Color scheme
+vim.cmd('colorscheme nightfox')
+vim.wo.signcolumn = 'yes'
+vim.wo.number = true
+vim.wo.relativenumber = false 
+
+-- Key mappings
+local map = vim.api.nvim_set_keymap
+map('n', '<c-s>', ':w<CR>', {})
+map('i', '<c-s>', '<Esc>:w<CR>', {})
+
+map('i', '<up>', '<nop>', {})
+map('i', '<down>', '<nop>', {})
+map('i', '<left>', '<nop>', {})
+map('i', '<right>', '<nop>', {})
+
+map('n', '<up>', '<nop>', {})
+map('n', '<down>', '<nop>', {})
+map('n', '<left>', '<nop>', {})
+map('n', '<right>', '<nop>', {})
+
+-- LSP stuff
+require('mason').setup()
+require('mason-lspconfig').setup()
+require('lsp')
+
+
+
+
---
+++ lua/lsp.lua
@@ -0,0 +1,123 @@
+-- Set up lsps -------------------------------------------------------
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+local set = vim.keymap.set
+set('n', '<space>e', vim.diagnostic.open_float, opts)
+set('n', '[d', vim.diagnostic.goto_prev, opts)
+set('n', ']d', vim.diagnostic.goto_next, opts)
+set('n', '<space>q', vim.diagnostic.setloclist, opts)
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(_, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    set('n', 'K', vim.lsp.buf.hover, bufopts)
+    set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts) vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    set('n', 'gr', vim.lsp.buf.references, bufopts)
+    set('n', '<space>f', function()
+        vim.lsp.buf.format { async = true }
+    end, bufopts)
+end
+
+local lsp_flags = {
+    -- This is the default in Nvim 0.7+
+    debounce_text_changes = 150,
+}
+
+local cmp = require('cmp')
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Set up lspconfig
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Set up each lsp server
+local lspconfig = require('lspconfig')
+
+lspconfig['pyright'].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    flags = lsp_flags,
+})
+
+lspconfig['sumneko_lua'].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    flags = lsp_flags,
+})
+
+lspconfig.sumneko_lua.setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {'vim'},
+      },
+    },
+  },
+})
